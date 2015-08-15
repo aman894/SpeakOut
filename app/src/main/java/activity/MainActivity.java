e2352e21e49170b81828cdd19d5e88a4042f298e
Tatwika <tatwika_13@yahoo.com>
2015-08-15 22:41:17 +0700

    pdf viewer

--- app/src/main/java/activity/MainActivity.java
+++ app/src/main/java/activity/MainActivity.java
@@ -1,5 +1,8 @@
 package activity;
 
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Environment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,8 +10,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.aman.myapplication.R;
+
+import java.io.File;
+import java.io.FilenameFilter;
 
 import activity.FragmentDrawer;
 
@@ -17,7 +25,9 @@ public class MainActivity extends AppCompatActivity implements FragmentDrawer.Fr
     private Toolbar mToolbar;
     private FragmentDrawer drawerFragment;
 
-    @Override
+    String[] pdflist;
+    File[] imagelist;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -31,10 +41,39 @@ public class MainActivity extends AppCompatActivity implements FragmentDrawer.Fr
                 getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
         drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), mToolbar);
         drawerFragment.setDrawerListener(this);
+
+
+            File images = Environment.getExternalStorageDirectory();
+            imagelist = images.listFiles(new FilenameFilter() {
+                public boolean accept(File dir, String name) {
+                    return ((name.endsWith(".pdf")));
+                }
+            });
+            pdflist = new String[imagelist.length];
+            for (int i = 0; i < imagelist.length; i++) {
+                pdflist[i] = imagelist[i].getName();
+            }
+            this.setListAdapter(new ArrayAdapter<String>(this,
+                    android.R.layout.simple_list_item_1, pdflist));
+        }
+
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        String path = imagelist[(int) id].getAbsolutePath();
+        openPdfIntent(path);
     }
 
+    private void openPdfIntent(String path) {
+        try {
+            final Intent intent = new Intent(MainActivity.this, Second.class);
+            intent.putExtra(PdfViewerActivity.EXTRA_PDFFILENAME, path);
+            startActivity(intent);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-    @Override
+        
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
